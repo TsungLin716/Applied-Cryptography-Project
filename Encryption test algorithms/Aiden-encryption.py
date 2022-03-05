26ffb85356655d04ea927e9277a634d06e906162
Aiden Lammert <36143661+aidenlammert@users.noreply.github.com>
2022-03-05 13:37:51 -0500

    Update Aiden-encryption.py

--- Encryption test algorithms/Aiden-encryption.py
+++ Encryption test algorithms/Aiden-encryption.py
@@ -1,13 +1,14 @@
 import random
 
 # get a message from user input, split it into a list
-message = input("Enter message: ")
-messageList = list(message)
+#message = input("Enter message: ")
+#message_list = list(message)
 
-cipher = "string"
-alphabet = [" ","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+cipher = ""
+alphabet = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+            "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 key = []
-probRandom = 0.5                # probability that a random character is appended
+prob_random = 0.5                # probability that a random character is appended
 
 # generate the key
 while len(key) < 27:
@@ -15,26 +16,54 @@ while len(key) < 27:
     if not (x in key):
         key.append(x)
 
+# 40 elements in this dictionary
+dictionary2 = ["lacrosses", "protectional", "blistered", "leaseback", "assurers", "frizzlers", "submerse",
+               "rankness", "moonset", "farcer", "brickyard", "stolonic", "trimmings", "glottic", "rotates", "twirlier",
+               "stuffer", "publishable", "invalided", "harshens", "tortoni", 'unlikely', "alefs", "gladding",
+               "favouring", "particulate", "baldpates", "changeover", "lingua", "proctological", "freaking",
+               "outflanked", "amulets", "imagist", "hyped", "pilfers", "overachiever", "clarence", "outdates",
+               "smeltery"]
+
+
+# generate a cipher with dictionary2
+def ciphergen():
+    cipher_list = []
+
+    # repeat until cipher is 500 characters
+    while len(cipher_list) < 500:
+        current_word = dictionary2[random.randint(0, 39)]           # pick a random entry in dictionary2
+        word_list = list(current_word)                              # convert current_word characters to list
+
+        if len(word_list) + len(cipher_list) < 500:             # there is room for the full word
+            cipher_list += word_list                            # append new word to existing cipher list
+        else:
+            length_diff = len(word_list) + len(cipher_list) - 500
+            cipher_list += word_list[:length_diff - 1]          # append word to cipher up to 500 total characters
+
+    # return cipher_list converted to a string
+    return ''.join(cipher_list)
+
 
 # encrypt the message
-# encKey: encryption key list, the list of ints 0->26 with randomize positions
-# encMessage: message to be encrypted with characters split into a list
-def encrypt(encKey, encMessage):
-    cipherList = []
+# enc_key: encryption key list, the list of ints 0->26 with randomize positions
+# enc_message: message to be encrypted with characters split into a list
+def encrypt(enc_key, enc_message):
+    cipher_list = []
 
-    while len(encMessage) > 0:
-        if random.uniform(0, 1) > probRandom:
-            cipherList.append(alphabet[random.randint(0, 26)])  # append random alphabet character to cipher
+    while len(enc_message) > 0:
+        if random.uniform(0, 1) > prob_random:
+            cipher_list.append(alphabet[random.randint(0, 26)])  # append random alphabet character to cipher
         else:
-            alphaPos = alphabet.index(encMessage[0])            # position of message character in alphabet
-            posInKey = encKey[alphaPos]                         # value in key list at pos of message char in alphabet
+            alpha_pos = alphabet.index(enc_message[0])           # position of message character in alphabet
+            pos_in_key = enc_key[alpha_pos]                      # value in key list at pos of message char in alphabet
 
-            cipherList.append(alphabet[posInKey])               # append char at alphabet position from key list
+            cipher_list.append(alphabet[pos_in_key])             # append char at alphabet position from key list
 
-            encMessage.pop(0)                                   # remove first char from message list
+            enc_message.pop(0)                                   # remove first char from message list
 
-    cipherString = ''.join(cipherList)                          # convert cipher list to string
+    cipher_string = ''.join(cipher_list)                         # convert cipher list to string
 
-    print("The cipher is: \n" + cipherString)
+    print("The cipher is: \n" + cipher_string)
 
-encrypt(key, messageList)
+print(ciphergen())
+#encrypt(key, message_list)
