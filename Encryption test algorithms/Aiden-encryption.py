c17b8f6863048bf2f81c642feed730b6b1c03b7b
Aiden Lammert <aiden.lammert@gmail.com>
2022-03-05 15:22:57 -0500

    added Dictionary 2 message generator

--- Encryption test algorithms/Aiden-encryption.py
+++ Encryption test algorithms/Aiden-encryption.py
@@ -1,8 +1,8 @@
 import random
 
 # get a message from user input, split it into a list
-#message = input("Enter message: ")
-#message_list = list(message)
+message = input("Enter message: ")
+message_list = list(message)
 
 cipher = ""
 alphabet = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
@@ -15,33 +15,6 @@ while len(key) < 27:
     x = random.randint(0, 26)
     if not (x in key):
         key.append(x)
-
-# 40 elements in this dictionary
-dictionary2 = ["lacrosses", "protectional", "blistered", "leaseback", "assurers", "frizzlers", "submerse",
-               "rankness", "moonset", "farcer", "brickyard", "stolonic", "trimmings", "glottic", "rotates", "twirlier",
-               "stuffer", "publishable", "invalided", "harshens", "tortoni", 'unlikely', "alefs", "gladding",
-               "favouring", "particulate", "baldpates", "changeover", "lingua", "proctological", "freaking",
-               "outflanked", "amulets", "imagist", "hyped", "pilfers", "overachiever", "clarence", "outdates",
-               "smeltery"]
-
-
-# generate a cipher with dictionary2
-def ciphergen():
-    cipher_list = []
-
-    # repeat until cipher is 500 characters
-    while len(cipher_list) < 500:
-        current_word = dictionary2[random.randint(0, 39)]           # pick a random entry in dictionary2
-        word_list = list(current_word)                              # convert current_word characters to list
-
-        if len(word_list) + len(cipher_list) < 500:             # there is room for the full word
-            cipher_list += word_list                            # append new word to existing cipher list
-        else:
-            length_diff = len(word_list) + len(cipher_list) - 500
-            cipher_list += word_list[:length_diff - 1]          # append word to cipher up to 500 total characters
-
-    # return cipher_list converted to a string
-    return ''.join(cipher_list)
 
 
 # encrypt the message
@@ -65,5 +38,4 @@ def encrypt(enc_key, enc_message):
 
     print("The cipher is: \n" + cipher_string)
 
-print(ciphergen())
-#encrypt(key, message_list)
+encrypt(key, message_list)
